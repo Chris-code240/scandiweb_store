17678757592ed83377e0b608e36292207ede4c65
Chris-code240 <duah.marfochristian@gmail.com>
2024-10-20 18:17:32 +0100

    It has to work

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   }).catch(e=>alert(JSON.stringify(e)))
   }
   return (
-    <Router basename='/scandiweb_store'>
+    <Router>
 
       <div>
         <Header deleteCallBack={deleteCallBack} />
